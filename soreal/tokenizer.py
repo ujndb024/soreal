4f71a348b8383dea72725670f03a11971d47665a
Soon <144966767+ujndb024@users.noreply.github.com>
2024-12-01 01:59:25 +0000

    added tokenizer class

--- soreal/tokenizer.py
+++ soreal/tokenizer.py
@@ -1,3 +1,18 @@
-from typing import List
+import os
+from logging import getLogger
+from typing import List, Literal
 from sentencepiece import SentencePieceProcessor
 
+logger = getLogger(__name__)
+
+
+class Tokenizer:
+    def __init__(self, model_path: str) -> None:
+        """
+        Args:
+            model_path: str; The path to the SentencePiece model file.
+        """
+        assert os.path.isfile(path=model_path), model_path
+
+        self.sentencepiece_processor = SentencePieceProcessor(model_path=model_path)
+        logger.info(f"Reloaded SentencePiece model from {model_path}")
